73abfe5b0b14c55ed550ea9edb399f7eb13d8525
gautham seshapalli <seshapalligautham500@gmail.com>
2025-09-13 16:32:41 +0530

    Time: 5 ms (65.4%), Space: 44.4 MB (17.06%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,24 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int duplicate = x;
-        int revNum = 0;
-        int ld = 0;
+        if (x < 0) return false; 
+
+        long revNum = 0;
+        int original = x;
 
         while (x != 0) {
-            ld = x % 10;
-
-            revNum = (revNum * 10) + ld;
-
-            x = x / 10;
+            int ld = x % 10;
+            revNum = revNum * 10 + ld;
+            x /= 10;
         }
-        if (duplicate < 0) {
-            return false;
-        }
-        if (duplicate == revNum) {
-            return true;
-        } else {
-            return false;
 
-        }
+        return revNum == original;
     }
 }
